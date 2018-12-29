35b586d1f40b308ae24557a14ad01c972bb7e79e
vasilis <vasiliss_b@hotmail.com>
2018-12-30 00:52:55 +0200

    init

---
+++ gatsby-config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  siteMetadata: {
+    title: `ΩΡΛ ΠΕΡΙΣΤΕΡΙ - Κωνσταντίνος Χ. Μπουκόνης - Χειρουργός Ωτορινολαρυγγολόγος Ενηλίκων και Παίδων`,
+    description: `Εξειδικευθείς εις Λονδίνο, Μεγάλη Βρετανία - Χειρουργός Ωτορινολαρυγγολόγος Περιστερι - ΤΗΛ: 2130575052 - Ιατρείο ΩΡΛ Περιστέρι Ενηλίκων και Παίδων - Δέχεται ιδιώτικά - Συνεργασία με όλες τις ιδιωτικές ασφαλιστικές - Λεωφόρος Κωνσταντινουπόλεως 63, Περιστερι - Ear Nose and Throat Surgeon - orl peristeri - Otorhinolaryngology Peristeri Athens`,
+    keywords: `ΩΡΛ ΠΕΡΙΣΤΕΡΙ, Ωτορινολαρυγγολόγος Περιστέρι, ιατρός ΩΡΛ, Orilas, Orl Peristeri, orila paidia, ΜΠΟΥΚΟΝΗΣ, boukonis, παίδο-ωρλ, ωριλάς, ΩΡΛ Αθήνα, ΩΡΛ Ίλιον, ΩΡΛ Πετρούπολη, ΩΡΛ Χαϊδάρι, ΩΡΛ Αιγάλεω, Ear Nose and Throat Surgeon, Otorhinolaryngology`,
+    author: `@Cons_ENTSurgeon`,
+    siteUrl: `https://boukonis-orl.gr`
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/favico.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-sass`,
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-sitemap`
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.app/offline
+    // 'gatsby-plugin-offline',
+  ],
+}
---
+++ src/components/topbar.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import Link from 'gatsby'
+import {
+  FaMapMarkerAlt,
+  FaPhone,
+  FaMobileAlt,
+  FaEnvelope,
+  FaFacebook,
+  FaTwitter,
+  FaGooglePlusG,
+} from 'react-icons/fa'
+
+const Topbar = () => (
+  <div className="top-bar sticky-top">
+    <div className="d-flex justify-content-lg-between justify-content-md-center align-items-center h-100 container-fluid">
+      <ul className="list-inline p-2">
+        <li className="list-inline-item">
+          <a className="d-flex align-items-center p-1 text-white" href="#google-map">
+            <FaMapMarkerAlt size={12} style={{marginRight:'5px'}}/>
+            <span>Λεωφόρος Κωνσταντινουπόλεως 63, Περιστέρι</span>
+          </a>
+        </li>
+        <li className="list-inline-item">
+          <a className="d-flex align-items-center p-1 text-white" href="tel:2130 575052">
+            <FaPhone size={12} style={{marginRight:'5px'}}/>
+            <span>2130 575052</span>
+          </a>
+        </li>
+        <li className="list-inline-item">
+          <a className="d-flex align-items-center p-1 text-white" href="tel:6947 556211">
+            <FaMobileAlt size={12} style={{marginRight:'5px'}}/>
+            <span>6947 556211</span>
+          </a>
+        </li>
+        <li className="list-inline-item">
+          <a
+            className="d-flex align-items-center p-1 text-white"
+            href="mailto:kostasboukonis@gmail.com"
+          >
+            <FaEnvelope size={14} style={{marginRight:'5px'}}/>
+            kostasboukonis@gmail.com
+          </a>
+        </li>
+      </ul>
+      <ul className="list-inline p-2 d-none d-lg-block">
+        <li className="list-inline-item">
+          <a
+            className="d-flex align-items-center p-1 text-white"
+            href="https://www.facebook.com/ConsultantENTSurgeonRhinologist"
+            target="_blank"
+          >
+            <FaFacebook size={20} style={{marginRight:'5px'}}/>
+          </a>
+        </li>
+        <li className="list-inline-item">
+          <a
+            className="d-flex align-items-center p-1 text-white"
+            href="https://twitter.com/Cons_ENTSurgeon"
+            target="_blank"
+          >
+            <FaTwitter size={20} style={{marginRight:'5px'}}/>
+          </a>
+        </li>
+        <li className="list-inline-item">
+          <a
+            className="d-flex align-items-center p-1 text-white"
+            href="https://plus.google.com/u/0/106223965710849406721"
+            target="_blank"
+          >
+            <FaGooglePlusG size={20} style={{marginRight:'5px'}}/>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+)
+
+export default Topbar
