9cb56b872aaa840b7b49cf83e0a3ee60d31a3a12
vasilis <vasiliss_b@hotmail.com>
2019-05-20 22:26:24 +0300

    update articles in home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,19 +6,58 @@ import About from '../components/home/about';
 import Clinic from '../components/home/clinic';
 import Coops from '../components/home/coops';
 import Surgeries from '../components/home/surgeries';
-import Articles from '../components/home/articles';
+import Article from '../components/article'
 import GMap from '../components/home/gmap';
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Αρχική" />
-    <Slider/>
-    <About/>
-    <Clinic/>
-    <Coops/>
-    <Surgeries/>
-    <Articles/>
-    <GMap/>
-  </Layout>
-)
-
+import { Container, Row, Col } from 'reactstrap'
+const IndexPage = ({ data }) => {
+  const { allMarkdownRemark: articles } = data
+  return(
+    <Layout>
+      <SEO title="Αρχική" />
+      <Slider/>
+      <About/>
+      <Clinic/>
+      <Coops/>
+      <Surgeries/>
+      <section className="about mt-5">
+        <Container>
+          <Row>
+            <Col md={12}>
+              <h3>'Αρθρα</h3>
+            </Col>
+          </Row>
+          <Row>
+            {articles.edges.map((article, i) => (
+              <Article key={i} article={article} />
+            ))}
+          </Row>
+        </Container>
+      </section>
+      <GMap/>
+    </Layout>
+  )
+}
+export const articlesQuery = graphql`
+  query IndexPageQuery {
+    allMarkdownRemark(limit: 1000) {
+      edges {
+        node {
+          id
+          excerpt
+          frontmatter {
+            title
+            slug
+            image {
+              childImageSharp {
+                resize {
+                  src
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
 export default IndexPage
