5ba460e21f31279a7482d93bbc91f8f268f4d1ab
vasilis <vasiliss_b@hotmail.com>
2019-06-16 02:08:25 +0300

    update node

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,7 +10,6 @@ const path = require('path')
 exports.createPages = ({ actions, graphql }) => {
     const { createPage, createRedirect } = actions;
 
-    createRedirect({ fromPath: 'https://www.boukonis-orl.gr/*', toPath: 'https://www.boukonis-orl.gr/:splat', isPermanent: true, force: true })
     createRedirect({ fromPath: 'https://boukonis-orl.gr/*', toPath: 'https://www.boukonis-orl.gr/:splat', isPermanent: true, force: true })
 
     const articleTemplate = path.resolve(`src/templates/article.js`);
