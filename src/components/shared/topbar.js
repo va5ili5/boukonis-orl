27583627194cf2df61ea2889814ec648cb8d35bf
unknown <vasiliss_b@hotmail.com>
2020-06-21 23:22:51 +0300

    add online appointment to header

--- src/components/shared/topbar.js
+++ src/components/shared/topbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import data from '../../../data/topbar';
-import { Container, ListGroupItem } from 'reactstrap';
+import data from '../../../data/topbar'
+import { Container, ListGroupItem, Button } from 'reactstrap'
 import {
   FaMapMarkerAlt,
   FaPhone,
@@ -10,27 +10,37 @@ import {
   FaTwitter,
   FaLinkedin,
 } from 'react-icons/fa'
-import { Link as RRNavLink } from 'gatsby';
+import { Link as RRNavLink } from 'gatsby'
 
 const Topbar = () => (
   <div className="top-bar sticky-top">
     <Container className="d-flex justify-content-lg-between align-items-center h-100">
       <ul className="d-flex list-inline p-2">
         <li className="d-none d-lg-inline-flex list-inline-item">
-          <ListGroupItem className="d-flex align-items-center p-1 contact-link border-0" tag={RRNavLink} to='./contact' >
-            <FaMapMarkerAlt size={12} style={{marginRight:'5px'}}/>
+          <ListGroupItem
+            className="d-flex align-items-center p-1 contact-link border-0"
+            tag={RRNavLink}
+            to="./contact"
+          >
+            <FaMapMarkerAlt size={12} style={{ marginRight: '5px' }} />
             <span>{data.contact.address}</span>
           </ListGroupItem>
         </li>
         <li className="list-inline-item">
-          <a className="d-flex align-items-center p-1 contact-link" href="tel:2130575052">
-            <FaPhone size={12} style={{marginRight:'5px'}}/>
+          <a
+            className="d-flex align-items-center p-1 contact-link"
+            href="tel:2130575052"
+          >
+            <FaPhone size={12} style={{ marginRight: '5px' }} />
             <span>{data.contact.phone}</span>
           </a>
         </li>
-        <li className="list-inline-item">
-          <a className="d-flex align-items-center p-1 contact-link" href="tel:6947556211">
-            <FaMobileAlt size={12} style={{marginRight:'5px'}}/>
+        <li className="d-none d-lg-inline-flex list-inline-item">
+          <a
+            className="d-flex align-items-center p-1 contact-link"
+            href="tel:6947556211"
+          >
+            <FaMobileAlt size={12} style={{ marginRight: '5px' }} />
             <span>{data.contact.mobile}</span>
           </a>
         </li>
@@ -39,9 +49,21 @@ const Topbar = () => (
             className="d-flex align-items-center p-1 contact-link"
             href="mailto:kostasboukonis@gmail.com"
           >
-            <FaEnvelope size={14} style={{marginRight:'5px'}}/>
+            <FaEnvelope size={14} style={{ marginRight: '5px' }} />
             {data.contact.email}
           </a>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <Button
+            tag="a"
+            target="_blank"
+            href="https://www.appointfix.com/5ba81ac9a264"
+            color="danger"
+          >
+            Ραντεβού On-line
+          </Button>
         </li>
       </ul>
       <ul className="list-inline p-2 d-none d-lg-block">
@@ -52,7 +74,7 @@ const Topbar = () => (
             target="_blank"
             rel="noopener noreferrer"
           >
-            <FaFacebook size={21} style={{marginRight:'5px'}}/>
+            <FaFacebook size={21} style={{ marginRight: '5px' }} />
           </a>
         </li>
         <li className="list-inline-item">
@@ -62,7 +84,7 @@ const Topbar = () => (
             target="_blank"
             rel="noopener noreferrer"
           >
-            <FaTwitter size={21} style={{marginRight:'5px'}}/>
+            <FaTwitter size={21} style={{ marginRight: '5px' }} />
           </a>
         </li>
         <li className="list-inline-item">
@@ -72,7 +94,7 @@ const Topbar = () => (
             target="_blank"
             rel="noopener noreferrer"
           >
-            <FaLinkedin size={21} style={{marginRight:'5px'}}/>
+            <FaLinkedin size={21} style={{ marginRight: '5px' }} />
           </a>
         </li>
       </ul>
